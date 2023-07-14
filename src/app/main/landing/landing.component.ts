6774388968384852662c15903361e51b63b72f41
Daniel Escalante <d.escalante@uniandes.edu.co>
2023-07-13 22:20:51 -0500

    Links and logos

--- src/app/main/landing/landing.component.ts
+++ src/app/main/landing/landing.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  techs:String[] = ["python", "java", "angular", "SQL", "javascript", "typescript", "github", "C", "HTML", "CSS", "bootstrap", "oracle"];
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
